0a2044363553a5c78d30b054d6caf2c9fb84e471
Lenovo <masumbox910@gmail.com>
2025-07-11 14:43:41 +0600

    My new file 3

--- lib/splash screens/signin_page.dart
+++ lib/splash screens/signin_page.dart
@@ -1,4 +1,6 @@
 
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/widget/screen_background.dart';
 class SigninScreen extends StatefulWidget {
@@ -9,60 +11,119 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: screen_background(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 80,),
-              Text('Get Started with',
-              style: Theme.of(context).textTheme.titleLarge),
-              SizedBox(height: 24,),
-              TextFormField(
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: 'Email',
-
-                ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 80,),
+                  Text('Get Started with',
+                  style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(height: 24,),
+                  TextFormField(
+                    controller: _emailController,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Email',
+                    ),
+                    validator: (String ? value){
+                      String email=value ?? '';
+                      if(EmailValidator.validate(email)==false){
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    }
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    controller: _passwordController,
+                    textInputAction: TextInputAction.done,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Password',
+                    ),
+                      validator: (String ? value){
+                        if((value?.length ?? 0)<=6){
+                          return 'Please enter your Password more then 6 character';
+                        }
+                        return null;
+                      }
+                  ),
+                  SizedBox(height: 16,),
+                  ElevatedButton(
+                      onPressed: _onTapSignInButton,
+                      child: Icon(Icons.arrow_circle_right_outlined)),
+                  SizedBox(height: 20,),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(onPressed: _onTapForgotButton, child: Text('Forgot Password',style: TextStyle(color: Colors.grey)),),
+                        RichText(text: TextSpan(
+                          text: "Don't have an account?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: 0.5,
+                            color: Colors.black
+                          ),
+                          children:[
+                            TextSpan(
+                              text: " Sign Up",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 0.5,
+                                color: Colors.green
+                              ),
+                              recognizer: TapGestureRecognizer()..onTap = _onTapSignupButton
+                            )
+                          ]
+                        )),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Password',
-                ),
-              ),
-              SizedBox(height: 16,),
-              ElevatedButton(
-                  onPressed: (){},
-                  child: Icon(Icons.arrow_circle_right_outlined)),
-              SizedBox(height: 20,),
-              TextButton(onPressed: (){}, child: Text('Forgot Password',style: TextStyle(color: Colors.grey)),),
-              RichText(text: TextSpan(
-                text: "Don't have an account?",
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 0.5,
-                  color: Colors.black
-                ),
-                children:[
-
-
-                ]
-              ))
-            ],
+            ),
           ),
         ),
       ),
 
     );
   }
+  void _onTapSignInButton(){
+    if(_formKey.currentState!.validate()){
+      print('Email: ${_emailController.text}');
+      print('Password: ${_passwordController.text}');
+    }
+  }
+  void _onTapForgotButton(){
+
+  }
+  void _onTapSignupButton(){
+
+  }
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+
 }
+
